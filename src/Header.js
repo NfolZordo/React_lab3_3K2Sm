41ab753e1eb453b5553f1a2a85eb1e96bde7e042
John Doe <79935610+NfolZordo@users.noreply.github.com>
2023-03-07 16:35:44 +0200

    HomeJsEnd

---
+++ src/Header.js
@@ -0,0 +1,67 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button
+} from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Contacts from './Pages/Contacts';
+import About from './Pages/About';
+import Blog from './Pages/Blog';
+import logo from "./logo192.png";
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                height="30"
+                width="30"
+                className="d-inline-block align-top"
+                alt="Logo"
+              /> React Site
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" style={{ justifyContent: 'space-between' }}>
+              <Nav>
+                <Nav.Link href="/"> Home </Nav.Link>
+                <Nav.Link href="/about"> About us </Nav.Link>
+                <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                <Nav.Link href="/blog"> Blog </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="me-sm-3"
+                />
+                <Button variant="outline-info">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div style={{ marginTop: '50px' }}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/blog" element={<Blog />} />
+            </Routes>
+          </Router>
+        </div>
+      </>
+    )
+  }
+}
---
+++ src/Pages/Blog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container, Col, Row, Card, ListGroup } from "react-bootstrap";
+
+class Blog extends Component {
+    render() {
+        return (
+            <Container>
+
+            </Container>
+        );
+    }
+}
+
+export default Blog;
